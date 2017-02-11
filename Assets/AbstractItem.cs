9f662e0ad4bb41c723b258d6c2f9c451fb0a42a0
Vu Le <vu@adoreboard.com>
2017-02-11 15:47:25 +0700

    Abstract Items

---
+++ Assets/AbstractItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class AbstractItem : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+
+		// Only do collision when game started
+		if (GameManager.gameState != GameManager.GameStates.STARTED) {
+			return;
+		}
+
+		if (coll.gameObject.tag == "Ball") {
+			GameManager gameManager = FindObjectOfType<GameManager> ();
+			gameManager.assignItem (this.gameObject);
+		} 
+		Destroy(gameObject);
+	}
+
+}
--- Assets/SpeedItem.cs
+++ Assets/SpeedItem.cs
@@ -2,32 +2,8 @@
 using System.Collections;
 using UnityEngine.Networking;
 
-public class SpeedItem : NetworkBehaviour {
+public class SpeedItem : AbstractItem {
 
 	public float speed = 1f;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnTriggerEnter2D(Collider2D coll) {
-
-		// Only do collision when game started
-		if (GameManager.gameState != GameManager.GameStates.STARTED) {
-			return;
-		}
-
-		if (coll.gameObject.tag == "Ball") {
-			GameManager gameManager = FindObjectOfType<GameManager> ();
-			gameManager.assignItem (this.gameObject);
-		} 
-		Destroy(gameObject);
-	}
 		
 }
